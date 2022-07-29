aed39579c79a5c53c774c6b54071a7746ccc7936
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2022-07-29 16:44:54 -0400

    first working prototype, not ready to be merged

--- compiler/Compiler.js
+++ compiler/Compiler.js
@@ -2,6 +2,16 @@ const CompilerError = require('./errors.js');
 const fs = require('fs');
 const chalk = require('chalk');
 
+function qcCheck(){
+    //Checks if Quick Compile setting is enabled
+    const config = fs.readFileSync('./ehxconf', 'utf8');
+    if (config.includes('-qc')) {
+        return true;
+    }
+    //if no file is found, assume it is disabled
+    return false;
+}
+
 function Compile(fileLoc, out) {
     if(!fileLoc) {
         CompilerError.NoInputFileException('No input file specified');
@@ -11,6 +21,17 @@ function Compile(fileLoc, out) {
     }
     if(!fs.existsSync(fileLoc)) {
         CompilerError.NonExistentInputFileException(`Input file '${fileLoc}' does not exist`);
+    }
+    if(qcCheck() === true) {
+        console.log(chalk.yellow('»» Quick Compile enabled'));
+        //get chunk size from config file
+        const config = fs.readFileSync('./ehxconf', 'utf8');
+        const configArr = config.split('--');
+        const chunkSize = configArr[1];
+        quickCompile(fileLoc, out, chunkSize);
+        return;
+    } else {
+        console.log(chalk.yellow('»» Quick Compile disabled'));
     }
     const hex = fs.readFileSync(fileLoc, 'utf8');
     const hexFiltered = hex.replace(/\s/g, '').replace(/\n/g, '');
@@ -69,6 +90,54 @@ function Decompile(fileLoc, out) {
         CompilerError.OutputFileWriteException(err.message);
     }
     console.log(chalk.green(`»» Decompiled ${fileLoc} to ${out}`));
+}
+
+function quickCompile(fileLoc, out, config) {
+    const hex = fs.readFileSync(fileLoc, 'utf8');
+    config = parseInt(config);
+    console.log(chalk.green(`»» Chunk size: ${config}`));
+    let hexArr;
+    //split every n bytes
+    if(config === 1) {
+        //don't split and return 0
+        return 0;
+    }
+
+    //split every 3 * n bytes
+    let charSplit = config * 3;
+
+    //create regex to split on
+    const regex = new RegExp(`.{1,${charSplit}}`, 'g');
+
+    //create an chunked array using the hex var and the chunk size
+    hexArr = hex.match(regex);
+    //check if length is 0
+    if(hexArr === null) {
+        CompilerError.IncorrectHexFormattingException('No hex characters found');
+    }
+
+    //parse each chunk
+    for(let i = 0; i < hexArr.length; i++) {
+        hexArr[i] = hexArr[i].replace(/\s/g, '').replace(/\n/g, '');
+        //hex regex that allows for lowercase hex
+        const hexRegex = /^[a-fA-F0-9]+$/;
+        //test individual hex bytes
+        if(!hexRegex.test(hexArr[i])) {
+            CompilerError.InvalidHexCharacterException(`Invalid hex byte '${hexArr[i]}'`);
+        }
+    }
+    console.log(chalk.green(`«« Parsed and verified ${hexArr.length} chunks`));
+    //convert to byte arr
+    const byteArr = Buffer.from(hexArr.join(''), 'hex');
+    console.log(chalk.green(`«« Converted to byte array`));
+    //write to file
+    try {
+        fs.writeFileSync(out, byteArr);
+    }
+    catch(err) {
+        CompilerError.OutputFileWriteException(err.message);
+    }
+    console.log(chalk.green(`»» Compiled ${fileLoc} to ${out}`));
 }
 
 module.exports = { Compile, Decompile };
